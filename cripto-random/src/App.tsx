8a24809f4394ae30e2263a1a8ab772b597c4d5ec
dixonAnatoAscencio <dixonjesusanato@gmail.com>
2024-08-05 22:01:42 -0500

    criptografico atmosferico

--- cripto-random/src/App.tsx
+++ cripto-random/src/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+ import { useState, useEffect } from 'react'
 import './App.css'
 
+//https://www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [number, setNumber] = useState(0)
+  
+  useEffect(() => {
+    
+    fetch('https://www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new')
+      .then(resp => resp.json())
+      .then(data => {
+        setNumber(data)
+      })
+    
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Random Number Generator</h1>
+      <h3>Random number: {number}</h3>
+     
     </>
   )
 }
