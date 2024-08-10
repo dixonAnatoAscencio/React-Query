28f38ca9d0d9786d41912f558a78c6844516b4ca
dixonAnatoAscencio <dixonjesusanato@gmail.com>
2024-08-10 00:25:55 -0500

    add useLabels hook y fadeIn tailwind

--- tanstack-query-issues/src/issues/components/LabelPicker.tsx
+++ tanstack-query-issues/src/issues/components/LabelPicker.tsx
@@ -1,27 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
-import { getLabels } from "../actions";
-
-
-
+import { LoadingSpinner } from "../../shared";
+import { useLabels } from "../hooks";
 
 export const LabelPicker = () => {
-
-
-  const labelsQuery = useQuery({
-    queryKey:['labels'],
-    queryFn: getLabels,
-  })
+  
+  const {labelsQuery} = useLabels();
 
   if(labelsQuery.isLoading){
-    return <div className="flex items-center justify-center">Espere...</div>
+    return <div className="flex items-center justify-center">
+      <LoadingSpinner />
+    </div>
   }
 
   return (
     <div className="flex flex-wrap gap-2 justify-center">
-    {labelsQuery.data?.map(label => (
+    {labelsQuery.data?.map((label )=> (
       <span
         key={label.id}
-        className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer text-white"
+        className=" animate-fadeIn px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer text-white"
         style={{ border: `1px solid #${label.color}` }}
       >
         {label.name}
---
+++ tanstack-query-issues/src/issues/hooks/useLabels.tsx
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { getLabels } from "../actions";
+
+//hook independiente 
+export const useLabels = () => {
+  
+    const labelsQuery = useQuery({
+    queryKey: ["labels"],
+    queryFn: getLabels,
+    staleTime: 1000*60*60, //1 hour
+  });
+
+  return {
+    labelsQuery
+  };
+
+  
+};
+
+
