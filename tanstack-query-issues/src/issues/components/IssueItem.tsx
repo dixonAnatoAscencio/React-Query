595a636211fcb167be31707efb9c1c03f7e3a287
dixonAnatoAscencio <dixonjesusanato@gmail.com>
2024-09-07 14:37:50 -0500

    prefetchQuery data

--- tanstack-query-issues/src/issues/components/IssueItem.tsx
+++ tanstack-query-issues/src/issues/components/IssueItem.tsx
@@ -1,6 +1,8 @@
 import { FiInfo, FiMessageSquare, FiCheckCircle } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 import { GithubIssue, State } from "../interfaces/issue.interface";
+import { getIssue } from "../actions";
+import { useQueryClient } from "@tanstack/react-query";
 
 interface Props {
   issue: GithubIssue;
@@ -8,10 +10,21 @@ interface Props {
 
 export const IssueItem = ({ issue }: Props) => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const prefetchData = () => {
+    queryClient.prefetchQuery({
+      queryKey: ["issue", issue.number],
+      queryFn: () => getIssue(issue.number),
+      staleTime: 1000 * 60,
+    });
+  };
 
   return (
-    <div className=" animate-fadeInflex items-center px-2 py-3 mb-5 border rounded-md bg-slate-900 hover:bg-slate-800">
-      
+    <div
+      onMouseEnter={prefetchData}
+      className=" animate-fadeInflex items-center px-2 py-3 mb-5 border rounded-md bg-slate-900 hover:bg-slate-800"
+    >
       {issue.state === State.Close ? (
         <FiCheckCircle size={30} color="green" className="min-w-10" />
       ) : (
